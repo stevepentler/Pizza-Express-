033aa966d2da7fca8d732e77f21e1c7a53556fea
Steve Pentler <stevepentler@gmail.com>
2016-04-17 10:08:46 -0600

    Initiates app & server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Pizza Steve Express';
+
+app.get('/', (request, response) => {
+  response.send('HI PIZZA STEVE');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
