b63f38eb6a0c1d9b42f68ead78ed313f8dd4bb30
Steve Pentler <stevepentler@gmail.com>
2016-04-17 12:05:40 -0600

    Adds redirect route

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const generateId = require('./lib/generate-id');
 app.use(express.static('static'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'handlebars');
+app.set('view engine', 'jade');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
@@ -19,18 +19,22 @@ app.get('/', (request, response) => {
   response.render('index');
 });
 
+app.post('/pizzas', (request, response) => {
+  if (!request.body.pizza) { return response.sendStatus(400); }
+
+  var id = generateId();
+  var pizza = request.body.pizza;
+  pizza.id = id;
+
+  app.locals.pizzas[id] = pizza;
+
+  response.redirect('/pizzas/' + id);
+});
+
 app.get('/pizzas/:id', (request, response) => {
   var pizza = app.locals.pizzas[request.params.id];
 
   response.render('pizza', { pizza: pizza });
-});
-
-app.post('/pizzas', (request, response) => {
-  var id = generateId();
-
-  app.locals.pizzas[id] = request.body;
-
-  response.sendStatus(201);
 });
 
 if (!module.parent) {
--- test/server-test.js
+++ test/server-test.js
@@ -64,7 +64,7 @@ describe('Server', () => {
     });
 
     it('should receive and store data', (done) => {
-      var payload = { pizza: fixtures.validPizza};
+      var validPizza = { pizza: fixtures.validPizza};
 
       this.request.post('/pizzas', { form: validPizza }, (error, response) => {
         if (error) { done(error); }
@@ -73,6 +73,17 @@ describe('Server', () => {
 
         assert.equal(pizzaCount, 1, `Expected 1 pizzas, found ${pizzaCount}`);
 
+        done();
+      });
+    });
+
+    it('should redirect the user to their new pizza', (done) => {
+      var payload = { pizza: fixtures.validPizza };
+
+      this.request.post('/pizzas', { form: payload }, (error, response) => {
+        if (error) { done(error); }
+        var newPizzaId = Object.keys(app.locals.pizzas)[0];
+        assert.equal(response.headers.location, '/pizzas/' + newPizzaId);
         done();
       });
     });
